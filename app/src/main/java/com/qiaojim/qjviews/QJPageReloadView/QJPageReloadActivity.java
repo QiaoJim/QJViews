c29f02eafd4ca1711fc7667190034a873edcab6c
QiaoJim <919106848@qq.com>
2018-01-04 13:34:23 +0800

    完善示例注释

--- app/src/main/java/com/qiaojim/qjviews/QJPageReloadView/QJPageReloadActivity.java
+++ app/src/main/java/com/qiaojim/qjviews/QJPageReloadView/QJPageReloadActivity.java
@@ -39,12 +39,18 @@ public class QJPageReloadActivity extends AppCompatActivity {
     private void initListener() {
         listener = new QJPageReloadView.QJPageReloadViewListener() {
 
+            /*
+            * 异步加载任务开始前回调，可准备自定义的提示UI
+            * main线程中，可直接操作更新UI*/
             @Override
             public void onStart() {
 //                Log.e(TAG, "======== onStart()回调 ==========" + Thread.currentThread().getName());
 
             }
 
+            /*
+            * 下拉刷新动作后回调
+            * 非UI线程，可执行耗时的加载任务*/
             @Override
             public boolean onRefresh(int totalCount) {
 //                Log.e(TAG, "======== onRefresh()回调 ==========" + Thread.currentThread().getName());
@@ -67,6 +73,9 @@ public class QJPageReloadActivity extends AppCompatActivity {
                 return true;
             }
 
+            /*
+            * 加载更多动作后回调
+            * 非UI线程，可执行耗时的加载任务*/
             @Override
             public boolean onLoadMore(int totalCount) {
 //                Log.e(TAG, "======== onLoadMore()回调 ==========" + Thread.currentThread().getName());
@@ -89,6 +98,10 @@ public class QJPageReloadActivity extends AppCompatActivity {
                 return true;
             }
 
+            /*
+            * 异步加载任务完成后回调，可控制自定义的提醒UI
+            * 即 onRefresh()、onLoadMore() return true后回调
+            * main线程中，可直接操作更新UI*/
             @Override
             public void onFinished() {
 //                Log.e(TAG, "======== onFinished()回调 ==========" + Thread.currentThread().getName());
@@ -107,6 +120,10 @@ public class QJPageReloadActivity extends AppCompatActivity {
         QJReloadViewAdapter adapter = new QJReloadViewAdapter(this, qjPageReloadView);
         adapter.setData(dataList);
         qjPageReloadView.setAdapter(adapter);
+
+        // 下一行demo会屏蔽xml中设置的自定义属性
+        // qjPageReloadView.setAutoLoadMore(false);
+
         qjPageReloadView.setQJPageReloadViewListener(listener);
 
     }
